579aee42d493cba0d804c6312ec418d2fb828e79
KhorKo <reach@KhorKo.local>
2026-02-13 09:54:29 +0700

    add /

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/byunn',
+  base: '/byunn/',
   plugins: [vue(), tailwindcss()],
 })
